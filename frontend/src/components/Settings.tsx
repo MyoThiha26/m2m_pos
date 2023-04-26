7e577d3bb8be5c9a2fe321f802723142dae17030
msquareprogramming <msquareprogramming@gmail.com>
2023-04-26 21:58:28 +0700

    feat: remember last selected location by using localStorage

--- frontend/src/components/Settings.tsx
+++ frontend/src/components/Settings.tsx
@@ -1,9 +1,67 @@
+import { useContext, useEffect, useState } from "react";
 import Layout from "./Layout";
+import { AppContext } from "../contexts/AppContext";
+import { Location } from "../typings/types";
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Box,
+  SelectChangeEvent,
+} from "@mui/material";
 
 const Settings = () => {
+  const { locations } = useContext(AppContext);
+  const [selectedLocation, setSelectedLocation] = useState<
+    Location | undefined
+  >();
+
+  useEffect(() => {
+    if (locations.length) {
+      const selectedLocationId = localStorage.getItem("selectedLocation");
+      if (!selectedLocationId) {
+        localStorage.setItem("selectedLocation", String(locations[0].id));
+        setSelectedLocation(locations[0]);
+      } else {
+        const selectedLocation = locations.find(
+          (location) => String(location.id) === selectedLocationId
+        );
+        setSelectedLocation(selectedLocation);
+      }
+    }
+  }, [locations]);
+
+  const handleOnchange = (evt: SelectChangeEvent<number>) => {
+    localStorage.setItem("selectedLocation", String(evt.target.value));
+    const selectedLocation = locations.find(
+      (location) => location.id === evt.target.value
+    );
+    setSelectedLocation(selectedLocation);
+  };
+
   return (
     <Layout>
-      <h1>Settings Page</h1>
+      <Box sx={{ maxWidth: 500, margin: "0 auto", mt: 5 }}>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Locations</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={selectedLocation ? selectedLocation.id : ""}
+            label="Locations"
+            onChange={handleOnchange}
+          >
+            {locations.map((location) => {
+              return (
+                <MenuItem key={location.id} value={location.id}>
+                  {location.name}
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
+      </Box>
     </Layout>
   );
 };
